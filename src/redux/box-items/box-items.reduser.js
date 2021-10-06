a06c6c0e5a9dec409e89773d04b5626e72be4edf
Артем Баторшин <norvellart@gmail.com>
2021-10-06 16:53:45 +0300

    started to change logic of reducers

--- src/redux/box-items/box-items.reduser.js
+++ src/redux/box-items/box-items.reduser.js
@@ -7,12 +7,62 @@ const INITIAL_STATE = {
 const boxItemsReduser = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'ADD_ITEM_IN_BOX':
-            return {
-                ...state,
-                boxItemsList: [...state.boxItemsList, { id: shortid.generate(), name: action.payload }]
+            switch (action.payload) {
+                case 'Text Input':
+                    return {
+                        ...state,
+                        boxItemsList: [...state.boxItemsList, {
+                            id: shortid.generate(),
+                            name: action.payload,
+                            isRequired: false,
+                            label: '',
+                            type: '',
+                            placeholder: '',
+                            sidebarIsHidden: true,
+                        }]
+                    }
+                case 'List':
+                    return {
+                        ...state,
+                        boxItemsList: [...state.boxItemsList, {
+                            id: shortid.generate(),
+                            name: action.payload,
+                            isRequired: false,
+                            label: '',
+                            isOrdered: 'true',
+                            numberOfElements: null,
+                            sidebarIsHidden: true,
+                        }]
+                    }
+                case 'Checkbox':
+                    return {
+                        ...state,
+                        boxItemsList: [...state.boxItemsList, {
+                            id: shortid.generate(),
+                            name: action.payload,
+                            isRequired: false,
+                            label: '',
+                            sidebarIsHidden: true,
+                        }]
+                    }
+                case 'Submit Button':
+                    return {
+                        ...state,
+                        boxItemsList: [...state.boxItemsList, {
+                            id: shortid.generate(),
+                            name: action.payload,
+                            value: '',
+                            sidebarIsHidden: true,
+                        }]
+                    }
+                default:
+                    return {
+                        ...state,
+                        boxItemsList: [...state.boxItemsList, { id: shortid.generate(), name: action.payload }]
+                    }
             }
         case 'REMOVE_ITEM':
-            return{
+            return {
                 ...state,
                 boxItemsList: state.boxItemsList.filter(boxItem => boxItem.id !== action.payload)
             }
